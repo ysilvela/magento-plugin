77c50f3490a38e20bfe1fdcc0b9310aac104e6cf
ysilvela <ysilvela@gmail.com>
2017-03-29 01:26:17 +0200

    Update Form.php

--- app/code/community/Mage/Coingate/Block/Form.php
+++ app/code/community/Mage/Coingate/Block/Form.php
@@ -1,10 +1,10 @@
 <?php
 
-class Mage_Coingate_Block_Form extends Mage_Payment_Block_Form
+class Mage_Eligebtc_Block_Form extends Mage_Payment_Block_Form
 {
     protected function _construct()
     {
-        $this->setTemplate('coingate/form.phtml');
+        $this->setTemplate('eligebtc/form.phtml');
 
         parent::_construct();
     }
